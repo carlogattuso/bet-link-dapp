a1151ab92f4b9acc6e6c1663646227447bb44abb
carlogattuso <carlogc46@gmail.com>
2023-10-01 01:44:55 +0200

    Initial Commit

---
+++ src/app/pages/home/home.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
+
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    HomeRoutingModule,
+    ButtonModule,
+    RippleModule
+  ]
+})
+export class HomeModule { }
